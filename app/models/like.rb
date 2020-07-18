af084e5662ca3ac9ba6733f4cc580e2ad0a85cfa
Saul Belisle <rbelisle1270@gmail.com>
2020-07-18 11:47:57 -0700

    app done added top posts

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
+  scope :popular, -> { select('post_id, count(post_id) as count').group(:post_id).order('count desc').limit(3) }
 end
